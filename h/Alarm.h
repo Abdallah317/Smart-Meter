ebb6c2b34e2405b8916f38b6bfffc69777663a8c
Abdallah317 <80751311+Abdallah317@users.noreply.github.com>
2025-06-30 23:27:08 +0300

    Update Alarm.h

--- h/Alarm.h
+++ h/Alarm.h
@@ -5,7 +5,6 @@
 #include <fstream>
 #include <ostream>
 #include <iostream>
-void Alarm(Filter_Signal s[], pp *f,int m, double Tsampling,int max_c,int max_v,int over_freq,int under_freq );
 void Freq_Alarm_List(Freq_Peri_List **Head, double Over_Freq, double Under_Freq);
 void Volt_Curr_Alarm_List(Read_data_List **Head, double max_c, double max_v,const string path);
 #endif // ALARM_H
