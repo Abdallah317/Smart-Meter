0fd40b7a405ad76a1dfbfb9f442b42406205a59a
Abdallah317 <80751311+Abdallah317@users.noreply.github.com>
2025-07-04 15:39:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "interface_list.h"
 int main(){
 /***************************************SMART METER****************************************/
+printf("Please if you see this message go to \"interface_list.cpp\" and edit path to yours!!\n");
 user_interface_List();
 return 0;
 }
